96b56b5ac984345320ef86db219b9621dc3f29a1
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-07-04 13:58:54 +0200

    first commit

--- test.lua
+++ test.lua
@@ -1,9 +1,9 @@
-local make = require "make"
+local erl = require "erl"
 local tokens, ast, value, vars, returning, err
 local text = io.open("test.txt", "r"):read("*a")
-tokens, err = make.lex("test.txt", text) if err then print(err) return end
+tokens, err = erl.lex("test.txt", text) if err then print(err) return end
 --if #tokens > 0 then for _, t in ipairs(tokens) do io.write(tostring(t)," ") end print("\n") end
-ast, err = make.parse(tokens) if err then print(err) return end
---print(make.ast2str(make.ast2str, ast))
-value, returning, err = make.interpret(ast) if err then print(err) return end
+ast, err = erl.parse(tokens) if err then print(err) return end
+--print(erl.ast2str(erl.ast2str, ast))
+value, returning, err = erl.interpret(ast) if err then print(err) return end
 if value.value ~= nil then print(value) end
