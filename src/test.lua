f96988368e401836039a0787b7389f4082bfff70
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2022-07-13 10:04:46 +0200

    test.lua fix

--- src/test.lua
+++ src/test.lua
@@ -4,15 +4,10 @@ local tokens, ast, value, returning, err
 local detail = false
 local text = io.open("test.rl", "r"):read("*a")
 tokens, err = rl.lex("test.rl", text) if err then print(err) return end
-local lexTime = os.clock()
 --if #tokens > 0 then for _, t in ipairs(tokens) do io.write(tostring(t)," ") end print("\n") end
 ast, err = rl.parse(tokens) if err then print(err) return end
-local parseTime = os.clock()
---print(rl.ast2str(rl.ast2str, ast))
-value, returning, err = rl.interpret(ast) if err then print(err) return end
-local runTime = os.clock()
-if returning or ast.name ~= "body" then print(value) end
-print("\nfinished in "..tostring(math.floor((lexTime+parseTime+runTime)*1000)).."ms")
-if detail then
-    print("(lex: "..tostring(lexTime).."s, parse: "..tostring(parseTime).."s, run: "..tostring(runTime).."s)")
+if ast then
+    --print(rl.ast2str(rl.ast2str, ast))
+    value, returning, err = rl.interpret(ast) if err then print(err) return end
+    if returning or ast.name ~= "body" then print(value) end
 end
